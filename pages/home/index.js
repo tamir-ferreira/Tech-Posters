2f8166d20cae87cb1368e360ea5c575ca82d01c4
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-09-22 19:42:54 -0300

    adicionada função botão "Seguir"

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,18 +1,14 @@
 const buttonsControllersModal = document.querySelectorAll("[data-control-modal]");
 const buttonsLike = document.querySelectorAll("[data-control-like]")
-// console.log(buttonsControllersModal)
-// console.log(buttonsLike)
-let count = [0,0,0];
+const buttonsFollow = document.querySelectorAll("[data-control-follow]")
+const count = [0,0,0];
 
 
 
 for(let i = 0; i < buttonsControllersModal.length; i++){
     buttonsControllersModal[i].addEventListener("click", ()=>{
-        // console.log("Click", i)
         let modalId = buttonsControllersModal[i].getAttribute("data-control-modal")
-        // console.log(modalId);
         document.getElementById(modalId).classList.toggle("show-modal");
-        // console.log(element);
     })
 }
 
@@ -20,8 +16,14 @@ for (let i = 0; i < buttonsLike.length; i++) {
     buttonsLike[i].onclick = (event) =>{
         count[i]++;
         event.target.parentElement.children[2].innerText = count[i]
-        // console.dir(event.target);
         event.target.src = "../../assets/img/heart.svg"
     }
-    
+}
+
+for (let i = 0; i < buttonsFollow.length; i++) {
+    buttonsFollow[i].onclick = () =>{
+        const pressed = buttonsFollow[i].classList.toggle('btn-pressed')
+        if (pressed) buttonsFollow[i].innerText = 'Seguindo';
+        else buttonsFollow[i].innerText = 'Seguir';
+    }
 }
