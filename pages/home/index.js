39cc3569c3eff36fea74348aa5d9b24efb9fe1e2
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-09-22 19:21:32 -0300

    atualizado textos / botões de like e abrir Post OK

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,2 +1,27 @@
-/* Desenvolva a lógica da página aqui */
+const buttonsControllersModal = document.querySelectorAll("[data-control-modal]");
+const buttonsLike = document.querySelectorAll("[data-control-like]")
+// console.log(buttonsControllersModal)
+// console.log(buttonsLike)
+let count = [0,0,0];
 
+
+
+for(let i = 0; i < buttonsControllersModal.length; i++){
+    buttonsControllersModal[i].addEventListener("click", ()=>{
+        // console.log("Click", i)
+        let modalId = buttonsControllersModal[i].getAttribute("data-control-modal")
+        // console.log(modalId);
+        document.getElementById(modalId).classList.toggle("show-modal");
+        // console.log(element);
+    })
+}
+
+for (let i = 0; i < buttonsLike.length; i++) {
+    buttonsLike[i].onclick = (event) =>{
+        count[i]++;
+        event.target.parentElement.children[2].innerText = count[i]
+        // console.dir(event.target);
+        event.target.src = "../../assets/img/heart.svg"
+    }
+    
+}
